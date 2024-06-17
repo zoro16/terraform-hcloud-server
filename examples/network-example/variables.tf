35b2b40889228453d0935735b650657c8e6a6da6
Mohamed Abdelgadir <mohamed@nooicelab.com>
2024-06-17 09:52:22 +0200

    feat: added new server types

--- examples/network-example/variables.tf
+++ examples/network-example/variables.tf
@@ -8,11 +8,11 @@ variable "server_type_map" {
   description = "This is a mapping for Hcloud's server types. The `key` is a combination of `[Processor Architecture]_[Number of vCores]_[RAM size]`. The `value` is Hcloud server type name."
   type        = map(string)
   default = {
-    "x86_1_2GB" : "cx11",  # Intel
-    "x86_2_4GB" : "cx21",  # Intel
-    "x86_2_8GB" : "cx31",  # Intel
-    "x86_4_16GB" : "cx41", # Intel
-    "x86_8_32GB" : "cx51", # Intel
+    # "x86_1_2GB" : "cx11",  # Intel
+    "x86_2_4GB" : "cx22",   # Intel
+    "x86_4_8GB" : "cx32",   # Intel
+    "x86_8_16GB" : "cx42",  # Intel
+    "x86_16_32GB" : "cx52", # Intel
 
     "x86_2_2GB" : "cpx11",   # AMD
     "x86_3_4GB" : "cpx21",   # AMD
